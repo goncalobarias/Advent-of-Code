6d0975992c25609944785155a79fd72ccfed42da
Gocho1234 <goncalo.barias@gmail.com>
2021-12-25 16:34:22 +0000

    day 6

--- 2021/day_8/day_8.py
+++ 2021/day_8/day_8.py
@@ -12,12 +12,44 @@ def treat_data(txt):
 def find_numbers(data):
     numbers = 0
     for output in data:
-        for j in range(len(data[1][1])): 
+        for j in range(len(output[1])): 
             if len(output[1][j]) in [2, 4, 3, 7]: numbers += 1
     
     return numbers
 
 
-txt = "input.txt"
+def find_numbers(data):
+    for output in data:
+        numbers = ['', '', '', '', '', '', '', '', '', '']
+        current_line = output[0].copy()
+        for j in range(len(current_line)):
+            if len(current_line[j]) == 2: 
+                numbers[1] += current_line[j]
+                current_line[j] = 1
+            elif len(current_line[j]) == 4: 
+                numbers[4] += current_line[j]
+                current_line[j] = 4
+            elif len(current_line[j]) == 3: 
+                numbers[7] += current_line[j]
+                current_line[j] = 7
+            elif len(current_line[j]) == 7: 
+                numbers[8] += current_line[j]
+                current_line[j] = 8
+            
+            elif 'e' not in current_line[j]: 
+                numbers[2] += current_line[j]
+                current_line[j] = 2
+            elif (('e' and 'b' in current_line[j]) 
+                    and (len(current_line[j]) == 5)):
+                numbers[3] += current_line[j]
+                current_line[j] = 3
+            elif len(current_line[j]) == 5:
+                numbers[5] += current_line[j]
+                current_line[j] = 5       
+        print(numbers)
+
+    return numbers
+
+txt = "test.txt"
 data = treat_data(txt)
 print(find_numbers(data))

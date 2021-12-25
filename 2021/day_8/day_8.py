d0c30fe255881c7a203f38962ba58dc5708440a6
Gocho1234 <goncalo.barias@gmail.com>
2021-12-25 18:20:46 +0000

    day 8

--- 2021/day_8/day_8.py
+++ 2021/day_8/day_8.py
@@ -18,38 +18,70 @@ def find_numbers(data):
     return numbers
 
 
-def find_numbers(data):
+def decipher(numbers, data): 
+    for dec in range(len(data)):
+        for num in numbers:
+            if sorted(list(data[dec])) == sorted(list(num)):
+                data[dec] = str(numbers.index(num))
+                break
+
+    return int(''.join(data))
+
+
+def find_numbers_part_2(data):
+    sumation = 0
     for output in data:
         numbers = ['', '', '', '', '', '', '', '', '', '']
-        current_line = output[0].copy()
-        for j in range(len(current_line)):
-            if len(current_line[j]) == 2: 
-                numbers[1] += current_line[j]
-                current_line[j] = 1
-            elif len(current_line[j]) == 4: 
-                numbers[4] += current_line[j]
-                current_line[j] = 4
-            elif len(current_line[j]) == 3: 
-                numbers[7] += current_line[j]
-                current_line[j] = 7
-            elif len(current_line[j]) == 7: 
-                numbers[8] += current_line[j]
-                current_line[j] = 8
+        current_line = output[0]
+        
+        # Going thru the list one time to find the digits 1, 7, 4 and 8.
+        for dig in current_line:
+            # Find digit 1
+            if len(dig) == 2: numbers[1] += dig
             
-            elif 'e' not in current_line[j]: 
-                numbers[2] += current_line[j]
-                current_line[j] = 2
-            elif (('e' and 'b' in current_line[j]) 
-                    and (len(current_line[j]) == 5)):
-                numbers[3] += current_line[j]
-                current_line[j] = 3
-            elif len(current_line[j]) == 5:
-                numbers[5] += current_line[j]
-                current_line[j] = 5       
-        print(numbers)
+            # Find digit 7
+            elif len(dig) == 3: numbers[7] += dig
 
-    return numbers
+            # Find digit 4
+            elif len(dig) == 4: numbers[4] += dig
 
-txt = "test.txt"
-data = treat_data(txt)
-print(find_numbers(data))
+            # Find digit 8
+            elif len(dig) == 7: numbers[8] += dig
+
+        # Viewing the list a second and last time to find the other digits.
+        for dig in current_line:
+            # Find digit 3
+            if (len(dig) == 5 and numbers[1][0] in dig 
+                 and numbers[1][1] in dig):
+                numbers[3] += dig
+
+            # Find digit 6
+            elif (len(dig) == 6 
+                  and ((numbers[1][0] in dig and numbers[1][1] not in dig) 
+                  or (numbers[1][1] in dig
+                  and numbers[1][0] not in dig))):
+                numbers[6] += dig
+
+            # Find digit 0
+            elif (len(dig) == 6 
+                 and len(list(l for l in dig if l not in numbers[4])) == 3): 
+                numbers[0] += dig
+
+            # Find digit 2
+            elif (len(dig) == 5 
+                  and len(list(l for l in dig if l not in numbers[4])) == 3):
+                numbers[2] += dig
+
+            # Find digit 9
+            elif (len(dig) == 6 
+                  and len(list(l for l in dig if l not in numbers[4])) == 2):
+                numbers[9] += dig
+            
+            # Find digit 5
+            elif (len(dig) == 5 
+                  and len(list(l for l in dig if l not in numbers[4])) == 2):
+                numbers[5] += dig
+    
+        sumation += decipher(numbers, output[1])
+
+    return sumation

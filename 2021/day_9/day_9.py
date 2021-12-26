21caef3bc5635a78bc6201395bde2738c602c8ba
Gocho1234 <goncalo.barias@gmail.com>
2021-12-26 16:06:37 +0000

    day 9

--- 2021/day_9/day_9.py
+++ 2021/day_9/day_9.py
@@ -1,50 +1,13 @@
 def treat_data(txt):
     with open(txt) as file: data = file.readlines()
     for line in range(len(data)):
-        data[line] = list(data[line][:len(data[line]) - 1])
-        data[line] = list(map(int, data[line]))
+        data[line] = data[line][:len(data[line]) - 1]
 
     return data
 
 
-def find_low_pp(data):
-    LD, LL, risk_lev = len(data), len(data[0]), 0
-    for line in range(LD):
-        for pp in range(LL):
-            back, front, top, bottom = False, False, False, False
-            if data[line][pp] == 9: continue
-
-            if (((pp - 1 >= 0) 
-                and (data[line][pp - 1] > data[line][pp]))
-                or pp - 1 < 0):
-                back = True
-
-            if (((pp + 1 < len(data[line])) 
-                and (data[line][pp + 1] > data[line][pp]))
-                or pp + 1 >= len(data[line])):
-                front = True
-
-            if (((line - 1 >= 0) 
-                and (data[line - 1][pp] > data[line][pp]))
-                or line - 1 < 0):
-                top = True
-
-            if (((line + 1 < len(data)) 
-                and (data[line + 1][pp] > data[line][pp]))
-                or line + 1 >= len(data)):
-                bottom = True
-
-            if back and front and top and bottom:
-                risk_lev = risk_lev + data[line][pp] + 1
-    
-    return risk_lev
-
-
-### PART 2 ###
-#from collections import deque
-
 def get_point_value(map, point):
-    return map[point[1]][point[0]]
+    return int(map[point[1]][point[0]])
 
 
 def find_surround_points(map, coord):
@@ -59,11 +22,30 @@ def find_surround_points(map, coord):
     return points
 
 
+### PART 1 ###
+
+def find_low_pp(data):
+    LD, LL, risk_lev = len(data), len(data[0]), 0
+    for line in range(LD):
+        for col in range(LL):
+            pp = (col, line)
+            points = find_surround_points(data, pp)
+            bigger_points = list(filter(lambda x: get_point_value(data, x) 
+                                > get_point_value(data, pp), points))
+            
+            if len(bigger_points) == len(points):
+                risk_lev = risk_lev + get_point_value(data, pp) + 1
+   
+    return risk_lev
+
+
+### PART 2 ###
+
 def check_points(map, point, blacklist): 
     size, waiting = 0, [point]
+    blacklist.append(waiting[0])
     while len(waiting) != 0:
         size += 1
-        blacklist.append(waiting[0])
         points = find_surround_points(map, waiting[0])
         waiting.remove(waiting[0])
         for point in points:
@@ -80,19 +62,14 @@ def check_basin_size(map):
     LL, CC, sizes, blacklist = len(map), len(map[0]), [], []
     for line in range(LL):
         for col in range(CC):
-            if map[line][col] == 9: 
-                points = find_surround_points(map, (col, line)) 
-                num_point, two_points = 0, [0, 0]
+            if get_point_value(map, (col, line)) == 9: 
+                points = find_surround_points(map, (col, line))
                 for pp in points:
-                    if num_point == 2: break
                     if (get_point_value(map, pp) != 9  
                         and pp not in blacklist):
-                        two_points[num_point] = pp
-                        num_point += 1
-                try: sizes += [check_points(map, two_points[0], blacklist)]
+                        point = pp
+                try: sizes += [check_points(map, point, blacklist)]
                 except: pass 
-                try: sizes += [check_points(map, two_points[1], blacklist)]
-                except: pass
     sizes.sort()
      
     return sizes[-1] * sizes[-2] * sizes[-3]

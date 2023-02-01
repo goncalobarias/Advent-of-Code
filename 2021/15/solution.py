9a3aa7a9ba43fbe8900ac475563eee776c97280a
Gocho1234 <goncalo.barias@gmail.com>
2023-02-01 02:13:06 +0000

    Add fixed day 15 2021

--- 2021/15/solution.py
+++ 2021/15/solution.py
@@ -12,24 +12,31 @@ for w in range(5):
                     data[i][j] + w + q - 1
                 ) % 9 + 1
 
-graph = Graph()
-for i in range(dim):
-    for j in range(dim):
-        index = dim * i + j
-        if i + 1 < dim:
-            graph.add_edge(index, index + dim, caves[i + 1][j])
-        if j + 1 < dim:
-            graph.add_edge(index, index + 1, caves[i][j + 1])
+
+def get_graph(g, d):
+    graph = Graph()
+    for i in range(d):
+        for j in range(d):
+            index = d * i + j
+            if i - 1 >= 0:
+                graph.add_edge(index, index - d, g[i - 1][j])
+            if j - 1 >= 0:
+                graph.add_edge(index, index - 1, g[i][j - 1])
+            if i + 1 < d:
+                graph.add_edge(index, index + d, g[i + 1][j])
+            if j + 1 < d:
+                graph.add_edge(index, index + 1, g[i][j + 1])
+    return graph
 
 
 # PART 1
 print(
     "Answer to part 1 is",
-    find_path(graph, 0, dim * (start_dim - 1) + start_dim - 1).total_cost,
+    find_path(get_graph(data, start_dim), 0, start_dim**2 - 1).total_cost,
 )
 
 # PART 2
 print(
     "Answer to part 2 is",
-    find_path(graph, 0, dim**2 - 1).total_cost,
+    find_path(get_graph(caves, dim), 0, dim**2 - 1).total_cost,
 )
